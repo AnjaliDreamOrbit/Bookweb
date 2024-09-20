ce71cf609e0d6f6b6626c78416bb44d2ad25490f
anjali <anjali@360NDCPC058>
2024-09-20 11:54:36 +0530

    Add project files.

---
+++ BookwebProject/Data/ApplicationDBContext.cs
@@ -0,0 +1,27 @@
+﻿using BookwebProject.Models;
+using Microsoft.EntityFrameworkCore; 
+
+namespace BookwebProject.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        //pass the connectionString 
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options) :base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }//create table
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                 new Category { Id = 2, Name = "Comedy", DisplayOrder = 2},
+                  new Category { Id = 3, Name = "History", DisplayOrder = 3},
+                   new Category { Id = 4, Name = "Scifi", DisplayOrder = 4}
+
+                );
+             
+        }
+
+    }
+}
